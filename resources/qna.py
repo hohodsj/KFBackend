a3e71493f769293909a0779a6c67bdd64ec030da
hohodsj <hohodsj@gmail.com>
2023-05-19 23:29:33 -0400

    Small fix (#16)
    * cron
    
    * fix
    
    * update sec
    
    * order fix
    
    * lower and remove whitespace
    
    * send email

--- resources/qna.py
+++ resources/qna.py
@@ -5,8 +5,24 @@ from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 from schemas import QNASchema
 from db import db
 from models import QnaModel
+import requests
+import os
 
 blp = Blueprint("QNA", __name__, description="QuestionAndAnswers")
+
+def send_email(to, subject, body):
+    domain = os.getenv("MAILGUN_DOMAIN")
+    api = os.getenv("MAILGUN_API_KEY")
+    print(f'domain:{domain}  api:{api} subject:{subject} body:{body}')
+    requests.post(
+        f'https://api.mailgun.net/v3/{domain}/messages',
+        auth=("api", api),
+        data={"from": 'Excited User <mailgun@{domain}>',
+            "to": [to],
+            "subject": subject,
+            "text": body})
+    
+    
 
 @blp.route("/questions")
 class Questions(MethodView):
@@ -16,11 +32,24 @@ class Questions(MethodView):
     
     @blp.arguments(QNASchema)
     def post(self, qna_data):
+        to = []
+        user1_email = os.getenv("USER1_EMAIL")
+        print(user1_email)
+        if user1_email:
+            to.append(user1_email)
+        if os.getenv("USER2_EMAIL"):
+            to.append(os.getenv("USER2_EMAIL"))
         if "id" in qna_data:
             qna_obj = QnaModel.query.get(qna_data["id"])
             if qna_obj and qna_data["answer"].lower().replace(" ","") == qna_obj.answer.lower().replace(" ",""):
+                subject = f'Question {qna_obj.id} PASS'
+                body = f'Kathleen answer {qna_data["answer"]} match actual answer {qna_obj.answer}'
+                send_email(to, subject, body)
                 return {'verified':True}
             elif qna_obj and qna_data["answer"] != qna_obj.answer:
+                subject = f'Question {qna_obj.id} FAIL'
+                body = f'Kathleen answer {qna_data["answer"]} NOT match actual answer {qna_obj.answer}'
+                send_email(to, subject, body)
                 print(f'Input {qna_data["answer"]} is not {qna_obj.answer}')
             else:
                 print(f'{qna_data} does not exists in db')
